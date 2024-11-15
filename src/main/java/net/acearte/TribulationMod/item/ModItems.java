d702ad32494996a78295a1fc2d620aedc158763e
Acearte <76490484+Acearte@users.noreply.github.com>
2024-11-15 21:58:54 +0300

    Add files via upload
    Sorting categories

--- src/main/java/net/acearte/TribulationMod/item/ModItems.java
+++ src/main/java/net/acearte/TribulationMod/item/ModItems.java
@@ -18,24 +18,15 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TribulationMod.MOD_ID);
 
-    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
-            () -> new Item(new Item.Properties()));
-
-    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
-            () -> new Item(new Item.Properties()));
-
-    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
+       public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
             () -> new MetalDetectorItem(new Item.Properties().durability(100)));
-
-
+    
     // Technologies
     public static final RegistryObject<Item> BOT_MODULE = ITEMS.register("bot_module",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> CONTROLLER = ITEMS.register("controller",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> CIRCUIT = ITEMS.register("circuit",
-            () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> RUBY_LED = ITEMS.register("ruby_led",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> WIRE = ITEMS.register("wire",
             () -> new Item(new Item.Properties()));
@@ -49,29 +40,63 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> RADIOACTIVE_CELL = ITEMS.register("radioactive_cell",
             () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> SAPPHIRE_LED = ITEMS.register("sapphire_led",
-            () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> CITRINE_ROD = ITEMS.register("citrine_rod",
-            () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> CITRINE_LED = ITEMS.register("citrine_led",
-            () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> RADIOACTIVE_MODULE = ITEMS.register("radioactive_module",
             () -> new Item(new Item.Properties()));
-
-    //TOOLS
-    public static final RegistryObject<Item> SAPPHIRE_HAMMER = ITEMS.register("sapphire_hammer",
-            () -> new PickaxeItem(ModTiers.SAPPHIRE, 1, 1f,
-                    new Item.Properties()));
+    public static final RegistryObject<Item> MUSHROOM_MODULE = ITEMS.register("",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> FROZEN_CORE = ITEMS.register("",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> FROZEN_MODULE = ITEMS.register("",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> TRANSFER_NODE = ITEMS.register("**",
+            () -> new Item(new Item.Properties()));
+    
     //GEMS
-    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby",
-            () -> new Item((new Item.Properties())));
-    public static final RegistryObject<Item> CITRINE = ITEMS.register("citrine",
-            () -> new Item((new Item.Properties())));
     public static final RegistryObject<Item> MOONSTONE = ITEMS.register("moonstone",
             () -> new Item((new Item.Properties())));
     public static final RegistryObject<Item> ONYX = ITEMS.register("onyx",
             () -> new Item((new Item.Properties())));
-
+    
+    //RUBY MISC
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby",
+            () -> new Item((new Item.Properties())));
+    public static final RegistryObject<Item> RUBY_LED = ITEMS.register("ruby_led",
+            () -> new Item(new Item.Properties()));
+    
+    //R.TOOLS
+    public static final RegistryObject<Item> RUBY_LIGHTSABER = ITEMS.register("ruby_lightsaber",
+            () -> new SwordItem(ModTiers.RUBY, 1, 1f, 
+                    new Item.Properties()));
+    
+    //CITRINE MISC
+    public static final RegistryObject<Item> CITRINE = ITEMS.register("citrine",
+            () -> new Item((new Item.Properties())));
+    public static final RegistryObject<Item> CITRINE_ROD = ITEMS.register("citrine_rod",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CITRINE_LED = ITEMS.register("citrine_led",
+            () -> new Item(new Item.Properties()));
+    
+    //C.TOOLS
+    public static final RegistryObject<Item> CITRINE_BORE = ITEMS.register("citrine_bore",
+            () -> new PickaxeItem(ModTiers.CITRINE, 1, 1f, 
+                    new Item.Properties()));
+    
+    //SAPHIRE MISC
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SAPPHIRE_LED = ITEMS.register("sapphire_led",
+            () -> new Item(new Item.Properties()));
+    
+    //S.TOOLS
+    public static final RegistryObject<Item> SAPPHIRE_HAMMER = ITEMS.register("sapphire_hammer",
+            () -> new PickaxeItem(ModTiers.SAPPHIRE, 1, 1f,
+                    new Item.Properties()));
+    
+    
+    
+    
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
